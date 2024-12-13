b6d6208b7710c2d96a293a4b012eb38482fbe20c
cuonghx <cuonghx@gmail.com>
2024-12-13 16:00:00 +0700

    Add get profile scripts

---
+++ honeycomb-intergration/src/4-get-user-profile.ts
@@ -0,0 +1,28 @@
+import { client, projectAddress, userKeypair } from "./constants";
+
+async function getUserProfile() {
+  const users = await client.findUsers({
+    wallets: [userKeypair.publicKey.toString()],
+  });
+
+  console.log(users.user[0]);
+  const user = users.user[0];
+
+  if (user) {
+    console.log("User not found");
+    return;
+  }
+
+  console.log("Found!", user);
+
+  const profiles = await client.findProfiles({
+    projects: [projectAddress],
+    userIds: [user.id],
+  });
+
+  const profile = profiles.profile[0];
+
+  console.log("Found!", profile);
+}
+
+getUserProfile();
--- honeycomb-intergration/src/constants.ts
+++ honeycomb-intergration/src/constants.ts
@@ -3,6 +3,9 @@ import createEdgeClient from "@honeycomb-protocol/edge-client";
 import path from "path";
 import fs from "fs";
 import * as web3 from "@solana/web3.js";
+import { config } from "dotenv";
+
+config();
 
 const API_URL = "https://edge.test.honeycombprotocol.com/";
 
@@ -24,3 +27,10 @@ export const userKeypair = web3.Keypair.fromSecretKey(
   )
 );
 
+export const projectAddress = process.env.PROJECT_ADDRESS;
+
+if (!projectAddress) {
+  throw new Error(
+    "Please provide a project address in the environment variable PROJECT_ADDRESS"
+  );
+}
